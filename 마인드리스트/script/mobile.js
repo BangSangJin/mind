65b92eb3eb36f97282c0b40fc032e4946be13a5c
BangSangJin <bsj5485sjs@naver.com>
2024-01-16 22:57:01 +0900

    first commit

---
+++ 마인드리스트/script/mobile.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    let vh = 0;
+
+useEffect(() => {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}, []);
+});
